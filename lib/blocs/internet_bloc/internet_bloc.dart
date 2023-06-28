77f2a37da5550c8368b8b3a51047b7e96c731a00
Awaizkazi <Owaizkazi789@gmail.com>
2023-06-28 17:19:57 +0530

    c

--- lib/blocs/internet_bloc/internet_bloc.dart
+++ lib/blocs/internet_bloc/internet_bloc.dart
@@ -1,11 +1,23 @@
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:internet_bloc/blocs/internet_bloc/internet_event.dart';
 import 'package:internet_bloc/blocs/internet_bloc/internet_state.dart';
 
 class InternetBloc extends Bloc<InternetEvent, InternetState> {
+  Connectivity _connectivity = Connectivity();
+
   InternetBloc() : super(InternetInitialState()) {
-    // todo Ye Check karra hota hai ki Konsa Event ane pe kya karna hai 
-    on<InternetLostEvent>((event, emit) => null);
-    on<InternetGainedEvent>((event, emit) => null);
+    // todo Ye Check karra hota hai ki Konsa Event ane pe kya karna hai
+    on<InternetLostEvent>((event, emit) => emit(InternetLostState()));
+    on<InternetGainedEvent>((event, emit) => emit(InternetGainedState()));
+
+    _connectivity.onConnectivityChanged.listen((result) {
+      if (result == ConnectivityResult.mobile ||
+          result == ConnectivityResult.wifi) {
+        add(InternetGainedEvent());
+      } else {
+        add(InternetLostEvent());
+      }
+    });
   }
 }
